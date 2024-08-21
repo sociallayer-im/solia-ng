b43e3e2fd67a0f2646e9c4b3bd234bb24d49877a
jiangplus <jiang.plus.times@gmail.com>
2024-08-21 11:33:57 -0300

    add api controller

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,2 +1,106 @@
+require "jwt"
+$hmac_secret = ENV["JWT_SECRET_KEY"]
+
+class AppError < StandardError
+end
+
+class AuthTokenError < StandardError
+end
+
 class ApiController < ApplicationController
+  include Pundit::Authorization
+
+  def check_address(addr)
+    (addr =~ /^0x[a-fA-F0-9]{40}$/) == 0
+  end
+
+  def check_address_or_email(addr)
+    check_address(addr) || (addr =~ URI::MailTo::EMAIL_REGEXP) == 0
+  end
+
+  def check_badge_domain_label(label)
+    (label =~ /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*$/) == 0
+  end
+
+  def check_badge_domain_label_and_length(label)
+    (label.length >= 4) && check_badge_domain_label(label)
+  end
+
+  def check_profile_username(handle)
+    /^[a-z0-9]+([\-]{1}[a-z0-9]+)*$/.match(handle).to_s == handle
+  end
+
+  def check_profile_username_and_length(handle)
+    handle.length >= 6 && check_profile_username(handle)
+  end
+
+  def sanitize_text(content)
+    content = Sanitize.fragment(content, Sanitize::Config::RELAXED)
+  end
+
+  def self.encode_token(id)
+    payload = {
+      id: id,
+      address_type: "wallet",
+      "https://hasura.io/jwt/claims": {
+        "x-hasura-default-role": "user",
+        "x-hasura-allowed-roles": ["user"],
+        "x-hasura-user-id": id.to_s,
+      }
+    }
+    auth_token = JWT.encode payload, $hmac_secret, "HS256"
+  end
+
+  def current_profile
+    return Profile.find_by(address: @address) if @address
+
+    begin
+      token = params[:auth_token]
+      decoded_token = JWT.decode token, $hmac_secret, true, { algorithm: "HS256" }
+      @profile_id = decoded_token[0]["id"]
+      @profile = Profile.find_by(id: @profile_id)
+    rescue Exception => e
+      Rails.logger.info e.message
+      nil
+    end
+  end
+
+  def current_profile!
+    return Profile.find_by(address: @address) if @address
+
+    raise AuthTokenError.new("missing auth_token") unless params[:auth_token]
+
+    begin
+      token = params[:auth_token]
+      decoded_token = JWT.decode token, $hmac_secret, true, { algorithm: "HS256" }
+      @profile_id = decoded_token[0]["id"]
+    rescue Exception => e
+      Rails.logger.info e.message
+      raise AuthTokenError.new(e.message)
+    end
+
+    @profile = Profile.find_by(id: @profile_id)
+    raise AppError.new("profile is not found") unless @profile
+
+    @profile
+  end
+
+  def pundit_user
+    current_profile!
+  end
+
+  rescue_from Pundit::NotAuthorizedError do |err|
+    Rails.logger.info err.message
+    render json: { result: "error", message: err.message }, status: 403
+  end
+
+  rescue_from AuthTokenError do |err|
+    Rails.logger.info err.message
+    render json: { result: "error", message: "invalid auth_token: #{err.message}" }, status: 403
+  end
+
+  rescue_from AppError do |err|
+    Rails.logger.info err.message
+    render json: { result: "error", message: err.message }, status: 400
+  end
 end
